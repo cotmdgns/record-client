8275aaea0a3925592a13370b5dd9a81a67d75b79
cotmdgns <qmflskzm@gmail.com>
2024-11-04 13:56:04 +0900

    11_04_1

--- src/api/shoppingSave.js
+++ src/api/shoppingSave.js
@@ -10,9 +10,6 @@ export const createShoppingSave = async (data) => {
 };
 // 유저 장바구니 삭제
 export const deleteShoppingSave = async (data) => {
-  console.log(data);
-  console.log(data.userCode);
-  console.log(data.productCode);
   return await instance.delete(
     `deleteShoppingSave?userCode=${data.userCode}&productCode=${data.productCode}`
   );
@@ -24,10 +21,13 @@ export const createShoppingSaveOrder = async (data) => {
 };
 // 결제페이지 들어가면 해당 정보 가져오기
 export const createShoppingSaveOrderView = async (code) => {
-  return await instance.get("createShoppingSaveOrder/" + code);
+  return await instance.get("createShoppingSaveOrderView/" + code);
 };
 // 유저가 결제페이지에서 바로 나갈때 or 취소할때
 export const createShoppingSaveOrderDelete = async (data) => {
+  console.log(data);
+  console.log(data.userCode);
+  console.log(data.productCode);
   return await instance.delete(
     `createShoppingSaveOrderDelete?userCode=${data.userCode}&productCode=${data.productCode}`
   );
--- src/pages/bodyMain/CreateOrder.js
+++ src/pages/bodyMain/CreateOrder.js
@@ -12,36 +12,40 @@ const CreateOrder = () => {
   const { CreateCode } = location.state || {};
 
   //////////////////////////////////////////////////////////// 1 ( 바로 결제하기 했을때 상황들 )
-  const [viewProduct, setViewProduct] = useState("");
+  const [viewProduct, setViewProduct] = useState(null);
   // 해당상품 보여주기
   const viewSaveProduct = async () => {
     const result = await createShoppingSaveOrderView(member?.userCode);
     setViewProduct(result.data);
-    console.log(result.data);
   };
 
   // 나가면 자동으로 삭제하기
   const saveOrderClean = async () => {
     await createShoppingSaveOrderDelete({
       userCode: member?.userCode,
-      product: viewProduct.productCode,
+      productCode: viewProduct?.productCode,
     });
   };
   ////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////// 2 ( 장바구니에서 결제페이지 넘어갔을때 상황 )
 
+  // 현재 진행상황
+  // 들어오면 삭제 완료 나가면 삭제완료
+  // 해당 정보 가져와서 화면단에 보여주기
+  // 결제하기 누르면 만들어지면서 마이페이지에 나오게끔 만들기
   ////////////////////////////////////////////////////////////
+
   useEffect(() => {
-    if (member !== null) {
+    if (member != null) {
       viewSaveProduct();
     }
   }, []);
 
   // 언마운트
   useEffect(() => {
-    return () => {
-      saveOrderClean();
-    };
+    // return () => {
+    //   saveOrderClean();
+    // };
   }, []);
   return (
     <div id="createOrderBody">
@@ -55,7 +59,7 @@ const CreateOrder = () => {
           <div>1. 이러쿵 저러쿵</div>
           <div>1. 이러쿵 저러쿵</div>
           <button>결제하기</button>
-          <button>결제취소</button>
+          <button onClick={saveOrderClean}>결제취소</button>
         </div>
       )}
       {/* 장바구니 결제페이지 */}
